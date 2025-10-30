b4140ac4ab0109dd57d6b1e326928d4654aaa2fc
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-10-30 03:47:07 +0300

    feat: update ApiConstants for new base URL and add timeout settings; add pretty_dio_logger dependency

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,3 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = 'https://api.example.com';
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const int connectionTimeout = 10000;
+  static const int receiveTimeout = 10000;
+  static const int sendTimeout = 10000;
 }
