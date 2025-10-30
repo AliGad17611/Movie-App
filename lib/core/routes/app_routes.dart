6d2cce8d5c61e82fae5ee6400ad49856e49ddf87
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-10-30 03:57:50 +0300

    feat: implement routing system with AppRoutes and Routes classes for navigation

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/core/routes/routes.dart';
+import 'package:movie_app/feature/details/presentation/views/movie_details_view.dart';
+import 'package:movie_app/feature/home/presentation/views/home_view.dart';
+
+class AppRoutes {
+  const AppRoutes._();
+  Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.home:
+        return MaterialPageRoute(builder: (context) => const HomeView());
+      case Routes.movieDetails:
+        return MaterialPageRoute(builder: (context) => const MovieDetailsView());
+      default:
+        return MaterialPageRoute(builder: (context) => const Scaffold(body: Center(child: Text('404 Not Found'))));
+    }
+  }
+}
