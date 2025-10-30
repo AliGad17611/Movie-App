70610821a4f286aa304b1d2fe5616c05d0d40a0a
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-10-31 01:30:05 +0200

    feat: integrate envied package for environment variable management and update API constants with obfuscated token

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,6 +1,16 @@
+import 'package:envied/envied.dart';
+part 'api_constants.g.dart';
+
+@Envied(path: 'env/.env')
 class ApiConstants {
-  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String baseUrl = 'https://api.themoviedb.org/3/';
   static const int connectionTimeout = 10000;
   static const int receiveTimeout = 10000;
   static const int sendTimeout = 10000;
+  @EnviedField(obfuscate: true,varName: 'API_TOKEN')
+  static final String apiToken = 'Bearer ${_ApiConstants.apiToken}';
+
+}
+class ApiEndpoints {
+  static const String getPopularMovies = 'movie/popular';
 }
---
+++ lib/feature/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,6 @@
+import 'package:movie_app/core/network/dio_factory.dart';
+
+class HomeRemoteDataSource {
+  final DioFactory dioFactory;
+  HomeRemoteDataSource(this.dioFactory);
+}
