cc9844000a83338375bd448cddf01faeb0cda914
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-11-01 03:50:59 +0200

    feat: integrate Sentry for error tracking and performance monitoring in the application

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:movie_app/core/di/injection_container.dart' as di;
 import 'package:movie_app/core/routes/app_routes.dart';
 import 'package:movie_app/movie_app.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,5 +19,14 @@ void main() async {
   await Hive.initFlutter();
   HiveAdapters.registerAdapters();
   await di.init();
-  runApp(MovieApp(appRoutes: AppRoutes.instance));
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://680d7b804f0d232acf84f126227f1549@o4510287221948416.ingest.us.sentry.io/4510287226863616';
+      options.tracesSampleRate = 1.0;
+    },  appRunner: () => runApp(
+      SentryWidget(
+        child: MovieApp(appRoutes: AppRoutes.instance),
+      ),
+    ),
+  );
 }
